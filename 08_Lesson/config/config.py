138dcce6bc0483cee519307d7b781fac360536f4
Vlad Shevchenko <shevchenko.vlad98@mail.ru>
2025-11-07 19:30:14 +0800

    08_lesson

---
+++ 08_Lesson/config/config.py
@@ -0,0 +1,10 @@
+# === КОНФИГУРАЦИЯ  ===
+LOGIN = "shevchenko.vlad98@mail.ru"
+PASSWORD = "skalolaz123"
+COMPANY_ID = "45404059-6993-4709-a893-9b10bd6a8403"
+BASE_URL = "https://api.yougile.com/api-v2"
+
+
+AUTH_URL = f"{BASE_URL}/auth/keys"
+COMPANIES_URL = f"{BASE_URL}/companies"
+COMPANY_INFO_URL = f"{BASE_URL}/companies/{COMPANY_ID}"
