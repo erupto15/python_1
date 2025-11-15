b9cdedbe7707d4ad4c79c2bf7dacc7a5f2f7a8c9
erupto15 <shevchenko.vlad98@mail.ru>
2025-11-16 00:15:33 +0800

    Update cart_page.py

--- cart_page.py
+++ cart_page.py
@@ -10,30 +10,30 @@ from selenium.webdriver.support import expected_conditions as EC
 @allure.severity
 class CartPage:
 
-    allure.step ("Запуск работы, ожидание 10 сек {driver}")
+    allure.step (f"Запуск работы, ожидание 10 сек {driver}")
     def __init__(self, driver):
         self.driver = driver
         self.wait = WebDriverWait(driver, 10)
 
-    allure.step ("Получение данных {ID}, {checkout}")
+    @allure.step(f"Получение данных {ID}, {checkout}")
     def click_checkout(self):
         locator = (By.ID, "checkout")
         button = self.wait.until(EC.element_to_be_clickable(locator))
         button.click()
 
-    allure.step ("Проверка корзины {CLASS_NAME}, {cart_item}") 
+    @allure.step (f"Проверка корзины {CLASS_NAME}, {cart_item}") 
     def get_cart_items(self):
         # Для проверки содержимого корзины (опционально)
         locator = (By.CLASS_NAME, "cart_item")
         return self.driver.find_elements(*locator)
 
-    allure.step ("ПОлучение кол-ва товаров в корзине {cart_items}, {return}")
+    @allure.step (f"ПОлучение кол-ва товаров в корзине {cart_items}")
     def get_cart_count(self):
         # Получить количество товаров в корзине. Возвращает целое число
         items = self.get_cart_items()
         return len(items)
 
-    allure.step ("Проверка пустой корзины {wait}, {CLASS_NAME}, {cart_item}, {return}")
+    @allure.step (f"Проверка пустой корзины {wait}, {CLASS_NAME}, {cart_item}")
     def is_empty(self):
         """
         Проверить, пуста ли корзина.
