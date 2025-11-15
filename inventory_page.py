27446b666b60eeb81c6ba1fae835c9a1908c8e84
erupto15 <shevchenko.vlad98@mail.ru>
2025-11-16 00:16:46 +0800

    Update inventory_page.py

--- inventory_page.py
+++ inventory_page.py
@@ -10,30 +10,30 @@ from selenium.webdriver.support import expected_conditions as EC
 @allure.severity
 class InventoryPage:
 
-    allure.step ("Ожидание 10 сек {wait}")
+    @allure.step (f"Ожидание 10 сек {wait}")
     def __init__(self, driver):
         self.driver = driver
         self.wait = WebDriverWait(driver, 10)
 
-    allure.step ("Добавить товар {add_backpack}")
+    @allure.step (f"Добавить товар {add_backpack}")
     def add_backpack(self):
         locator = (By.ID, "add-to-cart-sauce-labs-backpack")
         button = self.wait.until(EC.element_to_be_clickable(locator))
         button.click()
     
-    allure.step ("Добавить товар {add_bolt_tshirt}")
+    @allure.step (f"Добавить товар {add_bolt_tshirt}")
     def add_bolt_tshirt(self):
         locator = (By.ID, "add-to-cart-sauce-labs-bolt-t-shirt")
         button = self.wait.until(EC.element_to_be_clickable(locator))
         button.click()
 
-    allure.step ("Добавление карты {add_to_cart}")
+    @allure.step (f"Добавление карты {add_to_cart}")
     def add_onesie(self):
         locator = (By.ID, "add-to-cart-sauce-labs-onesie")
         button = self.wait.until(EC.element_to_be_clickable(locator))
         button.click()
 
-    allure.step ("Применение карты {shoping_cart}")
+    @allure.step (f"Применение карты {shoping_cart}")
     def go_to_cart(self):
         locator = (By.CLASS_NAME, "shopping_cart_link")
         link = self.driver.find_element(*locator)
