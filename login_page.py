05b9c889249dc08002277975c679ebdf406c4559
erupto15 <shevchenko.vlad98@mail.ru>
2025-11-16 10:19:53 +0800

    Improve Allure annotations in login_page.py
    Refactor Allure step descriptions for clarity and consistency.

--- login_page.py
+++ login_page.py
@@ -3,19 +3,18 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-@allure.title
-@allure.description
-@allure.feature
-@allure.severity
-class LoginPage:
 
-    @allure.step (f"Ожидание работы 10 сек {driver}")
+
+@allure.feature("Авторизация")
+class LoginPage:
+    """Страница входа в систему (Saucedemo)."""
+
     def __init__(self, driver):
         self.driver = driver
         self.wait = WebDriverWait(driver, 10)
 
-    @allure.step (f"Ввод имени в поле логина {ID}, {username}")
-    def enter_username(self, username):
+    @allure.step("Ввод имени пользователя в поле логина")
+    def enter_username(self, username: str):
         """
         Ввести имя пользователя в поле логина.
 
@@ -24,23 +23,23 @@ class LoginPage:
         """
         locator = (By.ID, "user-name")
         element = self.wait.until(EC.element_to_be_clickable(locator))
-        element.clear()  # Очистка поля
+        element.clear()
         element.send_keys(username)
 
-    @allure.step (f"Ввод пароля в поле ввода {ID}:{password}")
-    def enter_password(self, password):
-
-        """Ввести пароль в поле ввода.
+    @allure.step("Ввод пароля в поле ввода")
+    def enter_password(self, password: str):
+        """
+        Ввести пароль в поле ввода.
 
         Args:
             password (str): Пароль
         """
         locator = (By.ID, "password")
         element = self.wait.until(EC.element_to_be_clickable(locator))
-        element.clear()  # Очистка поля
+        element.clear()
         element.send_keys(password)
 
-    @allure.step (f"Нажатие кнопки входа {ID}:{button}")
+    @allure.step("Нажатие кнопки входа")
     def click_login(self):
         """
         Нажать кнопку входа в систему.
